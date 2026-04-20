be01d2e302bb947a713a0b81524083d9097fd85c
Nigecat <development@nigelq.com>
2026-04-20 16:05:12 +1000

    Add windows builds

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ set -x
 rm -r build
 uv build ./package/mask_propagation/Cutie --out-dir build
 uvx cibuildwheel ./package --only cp310-manylinux_x86_64 --output-dir build
-# uvx cibuildwheel ./package --platform windows --output-dir build
+uvx cibuildwheel ./package --only cp310-win_amd64 --output-dir build
